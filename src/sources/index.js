c6098f5f1f338b1f62ef0310b1ff0e359d8e85bb
vudinh158 <vudinhtran2312@gmail.com>
2025-04-03 01:19:58 +0700

    update

---
+++ src/sources/index.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelectorAll('.header__nav-link');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.forEach(navLink => navLink.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
